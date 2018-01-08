341e11c178de06612a4d2b6d1de9b72ed62c5c7a
JARS3N <JARS3N@users.noreply.github.com>
2018-01-08 12:48:20 -0500

    Update convert_asyr_to_xlsx.R

--- inst/scripts/convert_asyr_to_xlsx.R
+++ inst/scripts/convert_asyr_to_xlsx.R
@@ -1,2 +1,6 @@
 library(seastar)
-shiny::runApp(system.file("ExportAsyrToExcel",package="shinyStar"),launch.browser=TRUE)
+app<-shiny::shinyApp(
+  shinyStar::asyr_to_xl_ui(),
+  shinyStar::asyr_to_xl_server()
+  )
+shiny::runApp(app,launch.browser=TRUE)
