1f453dd970408a99cff14134372fd7c452ec47d1
JARS3N <JARS3N@users.noreply.github.com>
2018-01-08 13:24:37 -0500

    Update add_reprint_barcodes.R

--- R/add_reprint_barcodes.R
+++ R/add_reprint_barcodes.R
@@ -1,7 +1,6 @@
 add_reprint_barcodes<-function(){
  lines<- c(
-    "library(seastar)",
-    "shiny::runApp(system.file(\"rePrintBarcodes\",package=\"shinyStar\"),launch.browser=TRUE)"
+"shiny::runApp(seascripts::reprint_barcodes_app(),launch.browser=TRUE)"
   )
 path_loc<- file.path(system.file(package='seascripts',path='scripts'),'barcodes_reprint.R')
 writeLines(lines,path_loc)
