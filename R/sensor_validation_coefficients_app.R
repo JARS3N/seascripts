ac4e843c30f004079a43c8238d054eb664f9ef49
JARS3N <JARS3N@users.noreply.github.com>
2023-05-23 13:00:29 -0400

    Update sensor_validation_coefficients_app.R

--- R/sensor_validation_coefficients_app.R
+++ R/sensor_validation_coefficients_app.R
@@ -1,7 +1,8 @@
 sensor_validation_coefficients_app<-function(){
+    barval::app()
   library(seastar)
-  shiny::shinyApp(
-    shinyStar::coef_ui_app(),
-    shinyStar::coef_server_app()
-  )
+  #shiny::shinyApp(
+    #shinyStar::coef_ui_app(),
+    #shinyStar::coef_server_app()
+  #)
 }
