dc19affec2e6e532dfb16e532cefb7f972da7aa6
JARS3N <JARS3N@users.noreply.github.com>
2023-05-30 09:18:22 -0400

    Update sensor_validation_coefficients.R

--- inst/scripts/sensor_validation_coefficients.R
+++ inst/scripts/sensor_validation_coefficients.R
@@ -1,2 +1,2 @@
-shiny::runApp(barval::ui_app(),barval::server_app(),launch.browser=TRUE)      
-
+#shiny::runApp(barval::ui_app(),barval::server_app(),launch.browser=TRUE)      
+shiny::runApp(barval::app(),launch.browser = T)
