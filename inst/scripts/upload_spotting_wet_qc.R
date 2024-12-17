bdac881250f57c362425c9165dc4109bfeca25eb
JARS3N <JARS3N@users.noreply.github.com>
2024-12-17 14:20:21 -0500

    Update upload_spotting_wet_qc.R

--- inst/scripts/upload_spotting_wet_qc.R
+++ inst/scripts/upload_spotting_wet_qc.R
@@ -1,1 +1,2 @@
-shiny::runApp(seascripts::upload_spotting_wet_qc_app(),launch.browser=TRUE)
+#shiny::runApp(seascripts::upload_spotting_wet_qc_app(),launch.browser=TRUE)
+shiny::runApp(wetqc::ui(), wetqc::server(),launch.browser=TRUE)
