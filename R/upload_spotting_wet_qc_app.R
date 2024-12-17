044698b0eed25143d8598712445b8fd443464d3d
JARS3N <JARS3N@users.noreply.github.com>
2024-12-17 14:41:13 -0500

    Update upload_spotting_wet_qc_app.R

--- R/upload_spotting_wet_qc_app.R
+++ R/upload_spotting_wet_qc_app.R
@@ -1,7 +1,12 @@
-upload_spotting_wet_qc_app<-function(){
-library(seastar)
-shiny::shinyApp(
-  shinyStar::upload_wetqc_ui(),
-  shinyStar::upload_wetqc_server()
-)
+#upload_spotting_wet_qc_app<-function(){
+#library(seastar)
+#shiny::shinyApp(
+##  shinyStar::upload_wetqc_ui(),
+#  shinyStar::upload_wetqc_server()
+#)
+#}
+upload_spotting_wet_qc_app <- function() {
+  library(seastar)
+  library(wetqc)
+  shiny::shinyApp(wetqc::ui(), wetqc::server())
 }
